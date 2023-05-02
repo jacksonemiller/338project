c03c4df131f87917a95a632a1ec3ce4f3d8f40a2
jacksonemiller <71667842+jacksonemiller@users.noreply.github.com>
2023-05-01 20:12:06 -0500

    added chatbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import DefaultChatMsg from './components/Chatbox';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <DefaultChatMsg/>
       </header>
     </div>
   );

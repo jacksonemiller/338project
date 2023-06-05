6b57359b4bfc37b8cc0d3944b2e31302d1f5caab
jacksonemiller <71667842+jacksonemiller@users.noreply.github.com>
2023-06-05 01:37:17 -0500

    added bot code

--- 338server/hammondServer/chatbot/views.py
+++ 338server/hammondServer/chatbot/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from bot import go
 
 @csrf_exempt
 def index(request):
-    print("endpoint reached")
     if request.method == 'PUT':
         data = request.body.decode('utf-8')
-        return HttpResponse(data)
+        return HttpResponse(go(data))
     return HttpResponse("Hello, world. You're at the polls index.")
